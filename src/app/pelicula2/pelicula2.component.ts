a08cb7ac1b53f3c356a2645ec7a9ad3185e94c26
julio <gorilamc@gmail.com>
2019-09-14 18:30:40 -0500

    se agrega pelicula2 y se modifica el componente

--- src/app/pelicula2/pelicula2.component.ts
+++ src/app/pelicula2/pelicula2.component.ts
@@ -9,7 +9,27 @@ export class Pelicula2Component implements OnInit {
 
   constructor() { }
 
+  lugar:string="0";
+  precio:string = "0";
+  select:string[]=["CineColombia","CineMark","RoyalFilms"];
+
   ngOnInit() {
+    for (let index = 0; index < this.select.length; index++) {
+      this.lugar=this.select[index];
+      if (this.lugar=="CineColombia") {
+          this.precio="9000";
+        console.log("Wwwwwwwwwwwwwww");
+        
+      }
+      if (this.lugar=="CineMark") {
+         this.precio="7000";
+      } if (this.lugar=="RoyalFilms") {
+         this.precio="6000";
+      } 
+      
+    }
   }
+
+img1="./assets/img/it2.jpg"
 
 }
